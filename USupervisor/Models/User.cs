fafd2d5e6ed77b386f12c47f1735d0325a8c9b92
Keiron-Beadle <K.BEADLE-2018@hull.ac.uk>
2020-11-29 21:13:48 +0000

    -Starting work on MVVM
    I'm trying to get the idea of how this pattern is implemented and how it can work.

---
+++ USupervisor/Models/User.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace USupervisor.Models
+{
+    abstract class User : INotifyPropertyChanged
+    {
+        protected string name;
+        protected string email;
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                if (!string.IsNullOrWhiteSpace(value))
+                    name = value;
+                OnPropertyChanged("Name");
+            }
+        }
+
+        protected void DeleteUser()
+        {
+            //Add delete code 
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
